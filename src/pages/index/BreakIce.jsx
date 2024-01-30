7fd80dbad546bb6d767b984a9a96d2ac64f21ea8
Bruce Hsu <brucehsu1126@gmail.com>
2024-01-29 19:46:39 -0800

    Add 3D buttons

--- src/pages/index/BreakIce.jsx
+++ src/pages/index/BreakIce.jsx
@@ -6,33 +6,56 @@ const GetInTouch = () => {
   return (
     <section className="py-4 md:py-6">
       <div className="mx-auto flex w-10/12 flex-col gap-2 sm:gap-4 lg:gap-6">
-        <Header text={"Connect"} />
-        <div className="flex flex-row justify-center gap-5">
-          <a
-            href="mailto:brucehsu1126@gmail.com"
-            className="project flex h-12 w-12 items-center justify-center rounded-full border-2 border-black bg-white p-3 text-2xl text-white sm:h-16 sm:w-16 sm:text-4xl"
-          >
-            <i className="ri-mail-line block text-black"></i>
-          </a>
-          <a
-            href="https://www.github.com/bobandash"
-            target="_blank"
-            className="project flex h-12 w-12 items-center justify-center rounded-full border-2 border-black bg-white p-3 text-2xl text-white sm:h-16 sm:w-16 sm:text-4xl"
-            rel="noreferrer"
-          >
-            <i className="ri-github-fill block text-black"></i>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/brucehsu1126"
-            target="_blank"
-            className="project flex h-12 w-12 items-center justify-center rounded-full border-2 border-black bg-white p-3 text-2xl text-black sm:h-16 sm:w-16 sm:text-4xl"
-            rel="noreferrer"
-          >
-            <i className="ri-linkedin-box-fill block"></i>
-          </a>
-        </div>
+        <Header text={"Let's Connect"} isDesktop={true} />
+        <Header text={"Connect"} isMobile={true} />
+        <Icons />
       </div>
     </section>
+  );
+};
+
+const Icons = () => {
+  return (
+    <>
+      <div className="flex flex-row justify-center gap-5">
+        <a
+          href="mailto:brucehsu1126@ucla.edu"
+          className="pushable"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="shadow"></span>
+          <span className="edge"></span>
+          <span className="front">
+            <i className="ri-mail-line block text-3xl text-black"></i>
+          </span>
+        </a>
+        <a
+          href="https://www.github.com/bobandash"
+          className="pushable"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="shadow"></span>
+          <span className="edge"></span>
+          <span className="front">
+            <i className="ri-github-fill block text-3xl text-black"></i>
+          </span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/brucehsu1126"
+          className="pushable"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="shadow"></span>
+          <span className="edge"></span>
+          <span className="front">
+            <i className="ri-linkedin-box-fill block text-3xl text-black"></i>
+          </span>
+        </a>
+      </div>
+    </>
   );
 };
 
